ae09400fefb2fdf9bc505f99543267826eb9da7d
Micheal-Omale <Michaelomaleson@gmail.com>
2023-11-27 18:34:08 +0000

    second commit

--- src/views/FeatureView.vue
+++ src/views/FeatureView.vue
@@ -5,7 +5,7 @@ import featureSection from '../components/FeaturePageComp/featureSection.vue'
 import featureBank from '../components/FeaturePageComp/featureBank.vue';
 import featureCards from '../components/FeaturePageComp/featureCards.vue';
 import cta from '../components/LandingPageComp/cta.vue';
-import faq from '../components/landingpagecomp/faq.vue';
+import faq from '../components/LandingPageComp/faq.vue';
 </script>
 
 <template>
